a52fd5503edaa2c22628eff40819266f10a526cf
Philipleahy <philip.leahy@HOTMAIL.COM>
2024-04-23 19:40:24 +0000

    Simple Loops Final Commit Correct Error

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <cstdio>
 
 // mLab 1 Simple Loops
 
@@ -7,10 +8,10 @@ int t2 = 1;
 int next_term = t1 + t2;
 int max_terms = 8;
 
-int main()
+int main() 
 {
-    printf("Hello, Lab Progrm 1 \r\n");
-    printf("Fibonacchi sequence: %d, %d, ", t1 + t2);
+    printf("Hello, Lab Program 1\r\n");
+    printf("Fibonacchi Sequence: %d, %d, ", t1, t2);
     for(int i=3; i<=max_terms; i++){
         printf("%d, ", next_term);
         t1 = t2;
